7e0e4204ca6be0446acc6ac522ab31c493bb4cf6
andressantage <126884365+andressantage@users.noreply.github.com>
2023-03-17 10:42:52 -0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,147 @@
+function f()
+{
+    var d1 = document.getElementById("d1");
+    d1.addEventListener("click", c);
+    function c()
+    {
+        var dd = document.getElementsByTagName("button")[1].innerHTML;  
+        d1.innerHTML=dd;
+        d.innerHTML="";
+    }
+}
+
+for (let i = 1; i <= 2; i++) {
+    window[`d${i}`] = document.getElementById(`d${i}`);
+  }
+var m = document.getElementById("m");
+
+d1.addEventListener("click", c1);
+d2.addEventListener("click", c2);
+d3.addEventListener("click", c3);
+d4.addEventListener("click", c4);
+d5.addEventListener("click", c5);
+d6.addEventListener("click", c6);
+d7.addEventListener("click", c7);
+d8.addEventListener("click", c8);
+d9.addEventListener("click", c9);
+d10.addEventListener("click", c10);
+d11.addEventListener("click", c11);
+d12.addEventListener("click", c12);
+d13.addEventListener("click", c13);
+d14.addEventListener("click", c14);
+d15.addEventListener("click", c15);
+d16.addEventListener("click", c16);
+d17.addEventListener("click", c17);
+d18.addEventListener("click", c18);
+d19.addEventListener("click", c19);
+d20.addEventListener("click", c20);
+d21.addEventListener("click", c21);
+d22.addEventListener("click", c22);
+d23.addEventListener("click", c23);
+d24.addEventListener("click", c24);
+d25.addEventListener("click", c25);
+d26.addEventListener("click", c26);
+d27.addEventListener("click", c27);
+d28.addEventListener("click", c28);
+d29.addEventListener("click", c29);
+d30.addEventListener("click", c30);
+d31.addEventListener("click", c31);
+d32.addEventListener("click", c32);
+d33.addEventListener("click", c33);
+d34.addEventListener("click", c34);
+d35.addEventListener("click", c35);
+d36.addEventListener("click", c36);
+d37.addEventListener("click", c37);
+d38.addEventListener("click", c38);
+d39.addEventListener("click", c39);
+d40.addEventListener("click", c40);
+d41.addEventListener("click", c41);
+d42.addEventListener("click", c42);
+d43.addEventListener("click", c43);
+d44.addEventListener("click", c44);
+d45.addEventListener("click", c45);
+d46.addEventListener("click", c46);
+d47.addEventListener("click", c47);
+d48.addEventListener("click", c48);
+d49.addEventListener("click", c49);
+d50.addEventListener("click", c50);
+d51.addEventListener("click", c51);
+d52.addEventListener("click", c52);
+d53.addEventListener("click", c53);
+d54.addEventListener("click", c54);
+d55.addEventListener("click", c55);
+d56.addEventListener("click", c56);
+d57.addEventListener("click", c57);
+d58.addEventListener("click", c58);
+d59.addEventListener("click", c59);
+d60.addEventListener("click", c60);
+d61.addEventListener("click", c61);
+d62.addEventListener("click", c62);
+d63.addEventListener("click", c63);
+d64.addEventListener("click", c64);
+
+a=0
+function c1(){if(a===0){a=1;x=`d1`;m.innerHTML="Selecciono";}else{a=0;d1.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c2(){if(a===0){a=1;x=`d2`;m.innerHTML="Selecciono";}else{a=0;d2.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c3(){if(a===0){a=1;x=`d3`;m.innerHTML="Selecciono";}else{a=0;d3.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c4(){if(a===0){a=1;x=`d4`;m.innerHTML="Selecciono";}else{a=0;d4.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c5(){if(a===0){a=1;x=`d5`;m.innerHTML="Selecciono";}else{a=0;d5.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c6(){if(a===0){a=1;x=`d6`;m.innerHTML="Selecciono";}else{a=0;d6.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c7(){if(a===0){a=1;x=`d7`;m.innerHTML="Selecciono";}else{a=0;d7.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c8(){if(a===0){a=1;x=`d8`;m.innerHTML="Selecciono";}else{a=0;d8.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c9(){if(a===0){a=1;x=`d9`;m.innerHTML="Selecciono";}else{a=0;d9.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c10(){if(a===0){a=1;x=`d10`;m.innerHTML="Selecciono";}else{a=0;d10.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c11(){if(a===0){a=1;x=`d11`;m.innerHTML="Selecciono";}else{a=0;d11.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c12(){if(a===0){a=1;x=`d12`;m.innerHTML="Selecciono";}else{a=0;d12.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c13(){if(a===0){a=1;x=`d13`;m.innerHTML="Selecciono";}else{a=0;d13.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c14(){if(a===0){a=1;x=`d14`;m.innerHTML="Selecciono";}else{a=0;d14.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c15(){if(a===0){a=1;x=`d15`;m.innerHTML="Selecciono";}else{a=0;d15.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c16(){if(a===0){a=1;x=`d16`;m.innerHTML="Selecciono";}else{a=0;d16.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c17(){if(a===0){a=1;x=`d17`;m.innerHTML="Selecciono";}else{a=0;d17.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c18(){if(a===0){a=1;x=`d18`;m.innerHTML="Selecciono";}else{a=0;d18.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c19(){if(a===0){a=1;x=`d19`;m.innerHTML="Selecciono";}else{a=0;d19.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c20(){if(a===0){a=1;x=`d20`;m.innerHTML="Selecciono";}else{a=0;d20.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c21(){if(a===0){a=1;x=`d21`;m.innerHTML="Selecciono";}else{a=0;d21.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c22(){if(a===0){a=1;x=`d22`;m.innerHTML="Selecciono";}else{a=0;d22.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c23(){if(a===0){a=1;x=`d23`;m.innerHTML="Selecciono";}else{a=0;d23.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c24(){if(a===0){a=1;x=`d24`;m.innerHTML="Selecciono";}else{a=0;d24.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c25(){if(a===0){a=1;x=`d25`;m.innerHTML="Selecciono";}else{a=0;d25.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c26(){if(a===0){a=1;x=`d26`;m.innerHTML="Selecciono";}else{a=0;d26.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c27(){if(a===0){a=1;x=`d27`;m.innerHTML="Selecciono";}else{a=0;d27.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c28(){if(a===0){a=1;x=`d28`;m.innerHTML="Selecciono";}else{a=0;d28.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c29(){if(a===0){a=1;x=`d29`;m.innerHTML="Selecciono";}else{a=0;d29.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c30(){if(a===0){a=1;x=`d30`;m.innerHTML="Selecciono";}else{a=0;d30.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c31(){if(a===0){a=1;x=`d31`;m.innerHTML="Selecciono";}else{a=0;d31.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c32(){if(a===0){a=1;x=`d32`;m.innerHTML="Selecciono";}else{a=0;d32.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c33(){if(a===0){a=1;x=`d33`;m.innerHTML="Selecciono";}else{a=0;d33.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c34(){if(a===0){a=1;x=`d34`;m.innerHTML="Selecciono";}else{a=0;d34.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c35(){if(a===0){a=1;x=`d35`;m.innerHTML="Selecciono";}else{a=0;d35.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c36(){if(a===0){a=1;x=`d36`;m.innerHTML="Selecciono";}else{a=0;d36.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c37(){if(a===0){a=1;x=`d37`;m.innerHTML="Selecciono";}else{a=0;d37.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c38(){if(a===0){a=1;x=`d38`;m.innerHTML="Selecciono";}else{a=0;d38.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c39(){if(a===0){a=1;x=`d39`;m.innerHTML="Selecciono";}else{a=0;d39.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c40(){if(a===0){a=1;x=`d40`;m.innerHTML="Selecciono";}else{a=0;d40.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c41(){if(a===0){a=1;x=`d41`;m.innerHTML="Selecciono";}else{a=0;d41.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c42(){if(a===0){a=1;x=`d42`;m.innerHTML="Selecciono";}else{a=0;d42.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c43(){if(a===0){a=1;x=`d43`;m.innerHTML="Selecciono";}else{a=0;d43.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c44(){if(a===0){a=1;x=`d44`;m.innerHTML="Selecciono";}else{a=0;d44.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c45(){if(a===0){a=1;x=`d45`;m.innerHTML="Selecciono";}else{a=0;d45.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c46(){if(a===0){a=1;x=`d46`;m.innerHTML="Selecciono";}else{a=0;d46.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c47(){if(a===0){a=1;x=`d47`;m.innerHTML="Selecciono";}else{a=0;d47.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c48(){if(a===0){a=1;x=`d48`;m.innerHTML="Selecciono";}else{a=0;d48.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c49(){if(a===0){a=1;x=`d49`;m.innerHTML="Selecciono";}else{a=0;d49.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c50(){if(a===0){a=1;x=`d50`;m.innerHTML="Selecciono";}else{a=0;d50.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c51(){if(a===0){a=1;x=`d51`;m.innerHTML="Selecciono";}else{a=0;d51.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c52(){if(a===0){a=1;x=`d52`;m.innerHTML="Selecciono";}else{a=0;d52.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c53(){if(a===0){a=1;x=`d53`;m.innerHTML="Selecciono";}else{a=0;d53.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c54(){if(a===0){a=1;x=`d54`;m.innerHTML="Selecciono";}else{a=0;d54.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c55(){if(a===0){a=1;x=`d55`;m.innerHTML="Selecciono";}else{a=0;d55.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c56(){if(a===0){a=1;x=`d56`;m.innerHTML="Selecciono";}else{a=0;d56.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c57(){if(a===0){a=1;x=`d57`;m.innerHTML="Selecciono";}else{a=0;d57.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c58(){if(a===0){a=1;x=`d58`;m.innerHTML="Selecciono";}else{a=0;d58.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c59(){if(a===0){a=1;x=`d59`;m.innerHTML="Selecciono";}else{a=0;d59.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c60(){if(a===0){a=1;x=`d60`;m.innerHTML="Selecciono";}else{a=0;d60.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c61(){if(a===0){a=1;x=`d61`;m.innerHTML="Selecciono";}else{a=0;d61.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c62(){if(a===0){a=1;x=`d62`;m.innerHTML="Selecciono";}else{a=0;d62.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c63(){if(a===0){a=1;x=`d63`;m.innerHTML="Selecciono";}else{a=0;d63.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
+function c64(){if(a===0){a=1;x=`d64`;m.innerHTML="Selecciono";}else{a=0;d64.innerHTML=document.getElementById(x).innerHTML;window[x].innerHTML="";m.innerHTML="Lanzo";}}
